33448aacf652941d63c2723d02a7916efe61ca97
Garrett Kucinski <garrettryan@doejo.com>
2016-11-15 16:21:44 -0600

    added variable to save data read from file to be able to work with it

--- league_builder.py
+++ league_builder.py
@@ -1,5 +1,18 @@
 import csv
 
+# Define list of team names
+teams = {'Raptors': [], 'Sharks': [], 'Dragons': []}
+
+# Define function to read in the roster of players
+def distibute_players():
+    with open('soccer_players.csv') as file:
+        read_roster = csv.DictReader(file)
+        roster = list(read_roster)
+        print(roster)
+        # for player in roster:
+        #     for info in player:
+        #         print('{}: {}'.format(info, player[info]))
+
 # make sure script can't be executed when imported
 if __name__ == "__main__":
-    pass
+    distibute_players()
