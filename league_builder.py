d69464b111dd6fddf46a5aaf764e0b5edd7ad4c0
Garrett Kucinski <garrettryan@doejo.com>
2016-11-22 15:00:58 -0600

    split responsiblities out into seperate functions

--- league_builder.py
+++ league_builder.py
@@ -3,16 +3,42 @@ import csv
 # Define list of team names
 teams = {'Raptors': [], 'Sharks': [], 'Dragons': []}
 
-# Define function to read in the roster of players
-def distibute_players():
+def get_roster():
     with open('soccer_players.csv') as file:
         read_roster = csv.DictReader(file)
         roster = list(read_roster)
-        print(roster)
-        # for player in roster:
-        #     for info in player:
-        #         print('{}: {}'.format(info, player[info]))
+    return roster
+
+def find_good_players():
+    experienced_player_count = 0
+    roster = get_roster()
+    for player in roster:
+        if player['Soccer Experience'] == "YES":
+            experienced_player_count += 1
+
+    # print statement for debug purposes, remove in final code
+    # print(experienced_player_count)
+    return experienced_player_count
+
+# Define function to read in the roster of players
+def distribute_players():
+    roster = get_roster()
+    find_good_players()
+    max_player_count = len(roster)/len(teams)
+    print(max_player_count)
+    for team in teams:
+        for player in roster:
+            if len(teams[team]) <= max_player_count:
+                print(len(teams[team]))
+                teams[team].append(player['Name'])
+
+def print_teams():
+    distribute_players()
+    for team in teams:
+        print(teams[team])
+
+print_teams()
 
 # make sure script can't be executed when imported
 if __name__ == "__main__":
-    distibute_players()
+    distribute_players()
